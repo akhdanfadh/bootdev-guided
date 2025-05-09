1d51a425a343b1c5e4b2ec6b4827fd09c3f53277
akhdanfadh <me@akhdan.dev>
2025-05-09 19:17:34 +0900

    2.1 main script and initial textnode implementation

---
+++ 03-static-site/src/main.py
@@ -0,0 +1,14 @@
+from textnode import TextNode, TextType
+
+
+def main():
+    text_node_1 = TextNode("Hello, world!", TextType.NORMAL)
+    text_node_2 = TextNode("This is a test.", TextType.LINK, "https://www.google.com")
+
+    print(text_node_1)
+    print(text_node_2)
+    print(text_node_1 == text_node_2)
+
+
+if __name__ == "__main__":
+    main()
