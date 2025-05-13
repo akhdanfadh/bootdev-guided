534ee9bc01b6332e6d3acb5e910ea54d828c149e
akhdanfadh <me@akhdan.dev>
2025-05-13 11:05:16 +0900

    4.2.1 Add Line and Point classes for 2D graphics; implement line drawing functionality in Window class and update main.py to demonstrate usage.

--- 04-maze-solver/graphics.py
+++ 04-maze-solver/graphics.py
@@ -1,6 +1,41 @@
 from tkinter import BOTH, Canvas, Tk
 
 
+class Point:
+    """A point in 2D space.
+
+    Attributes:
+        x: The x-coordinate of the point in pixels. 0 is the left edge of the window.
+        y: The y-coordinate of the point in pixels. 0 is the top edge of the window.
+    """
+
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+
+
+class Line:
+    """A line in 2D space.
+
+    Attributes:
+        p1: The first point of the line.
+        p2: The second point of the line.
+    """
+
+    def __init__(self, p1: Point, p2: Point):
+        self.p1 = p1
+        self.p2 = p2
+
+    def draw(self, canvas: Canvas, fill_color: str = "black", width: int = 2):
+        """Draw the line on the canvas."""
+        canvas.create_line(
+            (self.p1.x, self.p1.y),
+            (self.p2.x, self.p2.y),
+            fill=fill_color,
+            width=width,
+        )
+
+
 class Window:
     def __init__(self, width: int, height: int):
         self.__running = False
@@ -24,3 +59,7 @@ class Window:
 
     def close(self):
         self.__running = False
+
+    def draw_line(self, line: Line, fill_color: str = "black", width: int = 2):
+        """Draw a line on the canvas."""
+        line.draw(self.__canvas, fill_color, width)
--- 04-maze-solver/main.py
+++ 04-maze-solver/main.py
@@ -1,8 +1,10 @@
-from graphics import Window
+from graphics import Line, Point, Window
 
 
 def main():
     win = Window(800, 600)
+    line = Line(Point(50, 100), Point(100, 50))
+    win.draw_line(line)
     win.wait_for_close()
 
 
