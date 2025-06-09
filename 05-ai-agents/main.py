a09661e0f38abc7a616a109b8250fcca42b7fd29
akhdanfadh <me@akhdan.dev>
2025-06-09 13:44:22 +0900

    5.1.6 change arg handling with argparse AND add verbosity toggle for console output

--- 05-ai-agents/main.py
+++ 05-ai-agents/main.py
@@ -1,5 +1,5 @@
+import argparse
 import os
-import sys
 
 from dotenv import load_dotenv
 from google import genai
@@ -7,27 +7,32 @@ from google.genai import types
 
 
 def main():
-    if len(sys.argv) != 2:
-        print(
-            "UserError: LLM prompt is not provided as CLI argument.\nExample usage: `python3 main.py 'what is 2+2?'`"
-        )
-        exit(1)
-    user_prompt = sys.argv[1]
+    # Load CLI arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("prompt", type=str, help="User prompt/question for the LLM")
+    parser.add_argument(
+        "--verbose",
+        action="store_true",
+        help="Print user prompt and token usage to console",
+    )
+    args = parser.parse_args()
 
+    # Load environment (LLM API key)
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
 
+    # Model calling
     client = genai.Client(api_key=api_key)
-
     model = "gemini-2.0-flash-001"
     messages = [
-        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+        types.Content(role="user", parts=[types.Part(text=args.prompt)]),
     ]
     response = client.models.generate_content(model=model, contents=messages)
 
-    print(f"Text: {response.text}")
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if args.verbose:
+        print(f"User prompt: {response.text}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
 if __name__ == "__main__":
