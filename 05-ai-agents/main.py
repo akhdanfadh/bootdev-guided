413ec35c0f659000da465f82426fd0809eb890b6
akhdanfadh <me@akhdan.dev>
2025-06-09 22:48:38 +0900

    5.3.1 add system prompt

--- 05-ai-agents/main.py
+++ 05-ai-agents/main.py
@@ -24,15 +24,20 @@ def main():
     # Model calling
     client = genai.Client(api_key=api_key)
     model = "gemini-2.0-flash-001"
+    system_prompt = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
     messages = [
         types.Content(role="user", parts=[types.Part(text=args.prompt)]),
     ]
-    response = client.models.generate_content(model=model, contents=messages)
+    response = client.models.generate_content(
+        model=model,
+        contents=messages,
+        config=types.GenerateContentConfig(system_instruction=system_prompt),
+    )
 
     if args.verbose:
-        print(f"User prompt: {response.text}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print(f"Response:\n{response.text}")
 
 
 if __name__ == "__main__":
