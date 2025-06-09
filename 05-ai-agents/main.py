1a51c9059bf1566d280b0ebd10c708287dc141cb
akhdanfadh <me@akhdan.dev>
2025-06-09 23:15:38 +0900

    5.3.2 add function declaration for get files info to llm

--- 05-ai-agents/main.py
+++ 05-ai-agents/main.py
@@ -5,6 +5,38 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
+from functions.schema import schema_get_files_info
+
+MODEL = "gemini-2.0-flash-001"
+SYSTEM_PROMPT = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
+AVAILABLE_FUNCTIONS = types.Tool(function_declarations=[schema_get_files_info])
+
+
+def call_llm(client: genai.Client, messages: list[types.Content], verbose: bool = False):
+    response = client.models.generate_content(
+        model=MODEL,
+        contents=messages,
+        config=types.GenerateContentConfig(
+            system_instruction=SYSTEM_PROMPT, tools=[AVAILABLE_FUNCTIONS]
+        ),
+    )
+
+    if verbose:
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        if response.function_calls:
+            for call in response.function_calls:
+                print(f"Calling function: {call.name}({call.args})")
+    print(f"Response: {response.text}")
+
 
 def main():
     # Load CLI arguments
@@ -23,21 +55,10 @@ def main():
 
     # Model calling
     client = genai.Client(api_key=api_key)
-    model = "gemini-2.0-flash-001"
-    system_prompt = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
     messages = [
         types.Content(role="user", parts=[types.Part(text=args.prompt)]),
     ]
-    response = client.models.generate_content(
-        model=model,
-        contents=messages,
-        config=types.GenerateContentConfig(system_instruction=system_prompt),
-    )
-
-    if args.verbose:
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    print(f"Response:\n{response.text}")
+    call_llm(client, messages, args.verbose)
 
 
 if __name__ == "__main__":
