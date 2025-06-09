c2dd04449840c3a6f4684b91d21a7f0dbc5e988f
akhdanfadh <me@akhdan.dev>
2025-06-09 13:28:42 +0900

    5.1.4 pass content/prompt as CLI arg

--- 05-ai-agents/main.py
+++ 05-ai-agents/main.py
@@ -1,17 +1,24 @@
 import os
+import sys
 
 from dotenv import load_dotenv
 from google import genai
 
 
 def main():
+    if len(sys.argv) != 2:
+        print(
+            "UserError: LLM prompt is not provided as CLI argument.\nExample usage: `python3 main.py 'what is 2+2?'`"
+        )
+        exit(1)
+    contents = sys.argv[1]
+
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
 
     client = genai.Client(api_key=api_key)
 
     model = "gemini-2.0-flash-001"
-    contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
     response = client.models.generate_content(model=model, contents=contents)
 
     print(f"Text: {response.text}")
