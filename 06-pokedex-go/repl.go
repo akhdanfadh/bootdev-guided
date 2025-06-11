01de6bda7e4c8d1c1025f1d3ad3f6fa66cbd3e19
akhdanfadh <me@akhdan.dev>
2025-06-11 10:40:41 +0900

    6.1.4 add REPL main function

--- 06-pokedex-go/repl.go
+++ 06-pokedex-go/repl.go
@@ -1,6 +1,25 @@
 package main
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		// prompt and user input
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		input := scanner.Text()
+
+		// clean input and show the first word as command
+		words := cleanInput(input)
+		fmt.Printf("Your command was: %s\n", words[0])
+	}
+}
 
 // Split the users input into "words" based on whitespace,
 // lowercase the input, and trim any leading or trailing whitespace.
